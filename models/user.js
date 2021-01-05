b9caceee13b5ca062559d28d5cddce3b2f82010c
Quan Do <ducquan1996@gmail.com>
2021-01-05 16:08:34 +0700

    Added config for db

--- models/user.js
+++ models/user.js
@@ -2,7 +2,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
     return this._applyTimezone(date, options).format('YYYY-MM-DD HH:mm:ss.SSS');
 };
-const sequelize = new Sequelize('xshadyrecords', 'sa', 'doducquan', {
+const config = require('../models/config');
+const sequelize = new Sequelize('xshadyrecords', config.username, config.password, {
     host: 'localhost',
     dialect: 'mssql',
     timezone: '+07:00'
