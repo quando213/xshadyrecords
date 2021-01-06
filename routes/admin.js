a643f15bb5cd7bc58cb1832740f5d85f3f5ee217
bui-hoang-02 <HoangBTTH2009033@fpt.edu.vn>
2021-01-06 15:37:15 +0700

    file genres vs file genresCreate

--- routes/admin.js
+++ routes/admin.js
@@ -2,10 +2,18 @@ const express = require('express');
 const router = express.Router();
 
 router.use(express.urlencoded({ extended: false }));
+
 const controller = require('../controllers/userAdminController');
+const genreController = require('../controllers/genreController');
 
 router.get('/', controller.getDashboard);
 
 router.get('/user', controller.list);
+
+router.get('/genre', genreController.list);
+
+router.get('/genre/create',genreController.create);
+
+
 
 module.exports = router;
