13afc30ae694e8e45d59e1480741c1ad5a3d1cdf
Quan Do <ducquan1996@gmail.com>
2021-01-06 15:21:57 +0700

    Fixed artist admin page

--- models/artist.js
+++ models/artist.js
@@ -2,7 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize');
 Sequelize.DATE.prototype._stringify = function _stringify(date, options) {
     return this._applyTimezone(date, options).format('YYYY-MM-DD HH:mm:ss.SSS');
 };
-const config = require('../models/artist');
+const config = require('../models/config');
 const sequelize = new Sequelize('xshadyrecords', config.username, config.password, {
     host: 'localhost',
     dialect: 'mssql',
